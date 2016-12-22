43a9c5290db9ef80320b1a89bce8e79f5db2431e
Aas-Suhendar <aas.suhendar@gmail.com>
2016-12-22 11:09:44 +0700

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// front
+Route::get('/','Front@index');
+Route::get('/products','Front@products');
+Route::get('/products/details/{id}','Front@product_details');
+Route::get('/products/categories','Front@product_categories');
+Route::get('/products/brands','Front@product_brands');
+Route::get('/blog','Front@blog');
+Route::get('/blog/post/{id}','Front@blog_post');
+Route::get('/contact-us','Front@contact_us');
+Route::get('/login','Front@login');
+Route::get('/logout','Front@logout');
+Route::get('/cart','Front@cart');
+Route::get('/checkout','Front@checkout');
+Route::get('/search/{query}','Front@search');
